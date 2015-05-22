0c1f29c7be6905ed3e23d9363d4009f05cf80b7a
sephhh <sephkramer@gmail.com>
2015-05-22 16:11:24 -0400

    add simple form for new campaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,4 +2,16 @@ class CampaignsController < ApplicationController
   def index 
     @campaigns = Campaign.all
   end 
+
+  def create
+    @campaign = Campaign.create(campaign_params)
+    binding.pry
+    redirect_to '/campaigns'
+  end
+
+
+  def campaign_params
+    params.require(:campaign).permit(:description, :photo_url, :deadline, :total, :receiver_id)
+  end
 end
+

761f011312e9a71f0964a406e5c4c3607f451fae
sephhh <sephkramer@gmail.com>
2015-05-26 14:07:53 -0400

    add panels, spruce up campaign partial

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,4 +2,17 @@ class Campaign < ActiveRecord::Base
   has_many :pledges
   has_many :givers, through: :pledges
   belongs_to :receiver, :class_name => "User"
+
+
+  def total_contributed
+    self.pledges.sum(:amount)
+  end
+
+  def amount_left
+    self.total - self.total_contributed 
+  end
+
+  def pretty_date
+    self.deadline.strftime("%B %d, %Y")
+  end
 end

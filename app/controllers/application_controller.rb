07fb54d6196d54028e9a0a07ac9f50aa31bfd461
sephhh <sephkramer@gmail.com>
2015-05-26 16:26:41 -0400

    added edit and update campaign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
+  before_action :authenticate!
 
   def current_user
     # session[:user_id] = nil
@@ -14,5 +15,13 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def authenticate!
+    unless logged_in?
+      redirect_to root_url, notice: "You must be logged in to do that buddy."
+    end
+  end
+
 
 end
+
+

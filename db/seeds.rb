ac6ca4fd9174be72b0f762bd08005cc8e74af8dc
sephhh <sephkramer@gmail.com>
2015-05-29 15:43:43 -0400

    tweak user and setup views, more logic, filter out pending campaigns

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ sam = User.create(name: "Sam", email: "sam@example.com", password: "flatiron")
 
 emily = User.create(name: "Emily", email: "emily@example.com", password: "flatiron")
 
-new_purse = Campaign.create(title: "I need a new fancy purse", photo_url: "http://content.purseblog.com/images/2010/05/Louis-Vuitton-Speedy.jpg", deadline: Time.now, total: 1000, description:"I reaaaally really really really want a new purse to help me look fancy out on the town blah blah blah blah blah." )
+new_purse = Campaign.create(title: "I need a new fancy purse", photo_url: "http://content.purseblog.com/images/2010/05/Louis-Vuitton-Speedy.jpg", deadline: Time.now+(1 * 60 * 60 * 24), total: 1000, description:"I reaaaally really really really want a new purse to help me look fancy out on the town.", status: "Pending" )
 
 new_purse.receiver_id = emily.id
 
@@ -27,14 +27,14 @@ new_purse.pledges << seph_pledge
 new_purse.pledges << sam_pledge
 
 
-new_tv = Campaign.create(title: "Big screen TV please!", photo_url: "http://www.samsung.com/us/2012-smart-blu-ray-player/img/tv-front.png", deadline: Time.now, total: 700, description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." )
+new_tv = Campaign.create(title: "Big screen TV please!", photo_url: "http://www.samsung.com/us/2012-smart-blu-ray-player/img/tv-front.png", deadline: Time.now+(7 * 60 * 60 * 24), total: 700, description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", status: "Pending" )
 new_tv.receiver_id = seph.id
 new_tv.save
 
 new_tv.pledges << seph_pledge
 new_tv.pledges << sam_pledge
 
-new_pool = Campaign.create(title: "Above Ground Pool", photo_url: "http://emeraldoutdoorliving.com/wp-content/uploads/2012/03/DoughBoySwimmingPools_PresitgeOv.jpg", deadline: Time.now, total: 1400, description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." )
+new_pool = Campaign.create(title: "Above Ground Pool", photo_url: "http://emeraldoutdoorliving.com/wp-content/uploads/2012/03/DoughBoySwimmingPools_PresitgeOv.jpg", deadline: Time.now+(2 * 60 * 60 * 24), total: 1400, description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", status: "Pending" )
 new_pool.receiver_id = sam.id
 new_pool.save
 
@@ -42,45 +42,20 @@ new_pool.pledges << seph_pledge
 new_pool.pledges << sam_pledge
 
 
-
-# franco = User.create(name: 'Franco', email: 'franco@example.com')
-# josh = User.create(name: 'Josh', email: 'josh@example.com')
-# simone = User.create(name: 'Simone', email: 'simone@example.com')
-
-# big_screen = Campaign.create(description: 'I need a new TV', photo_url: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0CAcQjRw&url=http%3A%2F%2Fwww.mymcpl.org%2Fblog-tags%2Ftelevision%2520shows&ei=vmxfVY_8G9LasAS0sYD4Bw&bvm=bv.93990622,d.cWc&psig=AFQjCNGMBC5psL9OuytoEzfE97Omn83kqQ&ust=1432403523265440')
+franco = User.create(name: "Franco", email: "franco@example.com", password: "flatiron")
+josh = User.create(name: "Josh", email: "josh@example.com", password: "flatiron")
+chad = User.create(name: "Chad", email: "chad@example.com", password: "flatiron")
 
 
+new_box = Campaign.create(title: "A big box with a bow on it", photo_url: "/gift-1.jpg", deadline: Time.now+(2 * 60 * 60 * 24), total: 50, description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", status: "Pending" )
+new_box.receiver_id = josh.id
+new_box.save
 
-# ActiveRecord::Schema.define(version: 20150522173432) do
+new_jeans = Campaign.create(title: "New skinny jeans", photo_url: "http://cdna.the-lust-list.co.uk/wp-content/uploads/2014/02/69E14GBLK_large.jpg", deadline: Time.now+(2 * 60 * 60 * 24), total: 100, description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", status: "Pending" )
+new_jeans.receiver_id = franco.id
+new_jeans.save
 
-#   create_table "campaigns", force: :cascade do |t|
-#     t.text     "description"
-#     t.string   "photo_url"
-#     t.datetime "deadline"
-#     t.integer  "total"
-#     t.datetime "created_at",  null: false
-#     t.datetime "updated_at",  null: false
-#     t.integer  "receiver_id"
-#   end
-
-#   create_table "pledges", force: :cascade do |t|
-#     t.integer  "amount"
-#     t.integer  "campaign_id"
-#     t.datetime "created_at",  null: false
-#     t.datetime "updated_at",  null: false
-#     t.integer  "giver_id"
-#   end
-
-#   add_index "pledges", ["campaign_id"], name: "index_pledges_on_campaign_id"
-
-#   create_table "users", force: :cascade do |t|
-#     t.string   "name"
-#     t.string   "email"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
-#   end
-
-# end
-
-
+new_car = Campaign.create(title: "A Tesla!", deadline: Time.now+(10 * 60 * 60 * 24), photo_url: "http://assets.inhabitat.com/wp-content/blogs.dir/1/files/2013/06/Tesla-Model-S-537x301.jpg", total: 25000, description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", status: "Pending" )
+new_car.receiver_id = chad.id
+new_car.save
 
